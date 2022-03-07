eb699b3f54613ffd160b06eb3b7fa6fba4e55ae3
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-06 18:03:59 -0800

    ObjectMaster assignment, practice filter and map

---
+++ ObjectMaster/js/ObjectMaster.spec.js
@@ -0,0 +1,43 @@
+// // module.exports = divisibleBy3
+// const ObjectMaster = require('./ObjectMaster');
+ 
+// describe('an array of pokémon objects where the id is evenly divisible by 3', () => {
+//     test('correctly returns the sum of two numbers', () => {
+//         expect(divisibleBy3()).toBe(4);
+//     });
+// });    
+// describe('an array of pokémon objects that are "fire" type', () => {
+//     test.skip('', () => {
+//         expect(add(5, 10)).toBe(15);
+//     });
+// });
+// describe('an array of pokémon objects that have more than one type', () => {
+//     test.skip('', () => {
+//         expect(add(5, 10)).toBe(15);
+//     });
+// });
+// describe('an array with just the names of the pokémon', () => {
+//     test.skip('', () => {
+//         expect(add(5, 10)).toBe(15);
+//     });
+// });
+// describe('an array with just the names of pokémon with an id greater than 99', () => {
+//     test.skip('', () => {
+//         expect(add(5, 10)).toBe(15);
+//     });
+// });
+// describe('an array with just the names of the pokémon whose only type is poison', () => {
+//     test.skip('', () => {
+//         expect(add(5, 10)).toBe(15);
+//     });
+// });
+// describe('an array containing just the first type of all the pokémon whose second type is "flying"', () => {
+//     test.skip('', () => {
+//         expect(add(5, 10)).toBe(15);
+//     });
+// });
+// describe('a count of the number of pokémon that are "normal" type', () => {
+//     test.skip('', () => {
+//         expect(add(5, 10)).toBe(15);
+//     });
+// });
