3928e24c7f53e1d47bc316a3425aafdf20280e79
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-12 00:10:32 -0800

    replaced all indexes with object's id

--- React/Todo-List/todo-list-copy/src/App.js
+++ React/Todo-List/todo-list-copy/src/App.js
@@ -8,29 +8,47 @@ function App() {
     const newTodo = {
       todo,
       complete : false,
+      id : Math.floor(Math.random() * 10000)
     };
     setTodoList([...todoList, newTodo]);
     console.log(todoList)
     console.log(todo)
     setTodo('');
   }
-  const handleToDeleteTodo = (idx) => {
-    // if using an index instead of id
-    const deleteTodo = todoList.filter((oneTodo, i) => {
-      return i !== idx
-    });
-    setTodoList(deleteTodo);
-    console.log(`${deleteTodo} has been deleted`,deleteTodo)
-  }
-  const toggleToComplete = (idx) => {
-    const updateTodo = todoList.map((todo,i) => {
-      if (idx === i) {
+  const handleToDeleteTodo = (id) => {
+    // returning an object
+    let checkTodo = [...todoList].filter((todoObj) => todoObj.id === id)
+    // filters through the list of object comparing id's,
+    // only return objects that don't match, excludes the give id
+    setTodoList(todoList.filter((todo)=> todo.id !== id))
+    console.log(`${checkTodo} has been deleted`,checkTodo)
+  };
+  // const handleToDeleteTodo = (idx) => {
+  //   // if using an index instead of id
+  //   const deleteTodo = todoList.filter((oneTodo, i) => {
+  //     return i !== idx
+  //   });
+  //   setTodoList(deleteTodo);
+  //   console.log(`${deleteTodo} has been deleted`,deleteTodo)
+  // }
+  const toggleToComplete = (id) => {
+    const updateTodo = todoList.map((todo) => {
+      if (id === todo.id) {
         todo.complete = !todo.complete;
       }
       return todo;
     })
     setTodoList(updateTodo);
   }
+  // const toggleToComplete = (idx) => {
+  //   const updateTodo = todoList.map((todo,i) => {
+  //     if (idx === i) {
+  //       todo.complete = !todo.complete;
+  //     }
+  //     return todo;
+  //   })
+  //   setTodoList(updateTodo);
+  // }
   return (
     <div className="App">
       <header>
@@ -41,19 +59,19 @@ function App() {
         <button type="submit">Add</button>
       </form>
       {
-        todoList.map((todo,i) => {
+        todoList.map((todo) => {
           const todoCompleted = [];
           if(todo.complete) {
             todoCompleted.push('line-through');
           }
           return (
             
-            <div key={i}>
+            <div key={todo.id}>
               <p style={{
               textDecoration: todoCompleted
             }}>{todo.todo}</p>
-              <input type="checkbox" checked={todo.complete} onChange={(e) => toggleToComplete(i)}/>
-              <button onClick={(e) =>handleToDeleteTodo(i)}>Delete</button>
+              <input type="checkbox" checked={todo.complete} onChange={(e) => toggleToComplete(todo.id)}/>
+              <button onClick={(e) =>handleToDeleteTodo(todo.id)}>Delete</button>
               
             </div>
           )
