dd48d484094c7da27076fe64b5c7dc7769a28fb3
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-12 17:11:18 -0800

    can now toggle display an object's info

--- React/Tabs/tabs/src/Components/Display.js
+++ React/Tabs/tabs/src/Components/Display.js
@@ -1,34 +1,31 @@
 import React, {useState} from 'react';
 import '../App.css';
 const Display = (props) => {
-    const {tabList,toggleTab, setToggleTab} = props;
+    const {tabList,setTabList,toggleTab, setToggleTab} = props;
     // on click, initiate toggle function, show description if toggle is true
-        const toggle = (e,tab) => {
-            e.preventDefault();
-            console.log(tab)
-            if (toggleTab === false) {
-                setToggleTab(true);
-                console.log(toggleTab)
-            } else {
-                setToggleTab(false);
-                console.log(toggleTab)
-            }
-        }
-    // const onClickHandler = (e,value) => {
-    //     alert(`${value.tabName} ${value.description}`);
-    //     // using dot notation to pull out the value data seperatly
-    //     console.log(value);
-    // }
+    const toggleShow = (id) => {
+        const showTodoInfo = tabList.map((tab) => { // after mapping through list, find tab that matches id
+        // if the tab matches id, set show to true will display tab.description
+        console.log(`${tab} matches the ${id}`)
+        if (id === tab.id) {
+            tab.show = !tab.show; // 
+            console.log(tab.show)
+          }
+          return tab;
+        })
+        setTabList(showTodoInfo);
+        console.log(showTodoInfo)
+      }
     return tabList.map((tab) => {
         return (
             <div key={tab.id}> 
                 <hr/>
-                {/* <button onClick={(e) => onClickHandler(e, tab)}>{tab.tabName}</button> */}
-                <button onClick={(e) => toggle(e,tab)}>{tab.tabName}</button>
+                {/* checked function will set the object to have a false value, on click will change to the opposite  */}
+                <button checked={tab.show} onClick={(e) => toggleShow(tab.id)}>{tab.tabName}</button>
                 {
-                    toggleTab?
-                    <p>{tab.description}</p>
-                    :null
+                    tab.show?
+                    <p>{tab.description}</p> // if tab.show == false, return tab's description
+                    :null // if tab.show == false, return null
                 }
             </div>
             
