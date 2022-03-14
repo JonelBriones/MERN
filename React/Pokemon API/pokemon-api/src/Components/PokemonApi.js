426f829a047c5655f5a8cf0bcd1a04045a7a45ea
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-14 00:24:05 -0700

    added validations to previous and next buttons

--- React/Pokemon API/pokemon-api/src/Components/PokemonApi.js
+++ React/Pokemon API/pokemon-api/src/Components/PokemonApi.js
@@ -2,27 +2,75 @@ import React, {useEffect, useState} from 'react';
 import axios from 'axios'
 const PokemonApi = (props) => {
 
-    const [pokemon,setPokemon] = useState([]);
+    const [pokemon,setPokemon] = useState({});
+    const [search,setSearch] = useState(1);
     useEffect(()=>{
         axios
-        .get('https://pokeapi.co/api/v2/pokemon?limit=807')
+        
+        .get(`https://pokeapi.co/api/v2/pokemon/${search}`)
         // .then(response=>{setPokemon(response.data.results)})
         .then((response) => {
-            setPokemon(response.data.results);
-            console.log(pokemon)
+            setPokemon(response.data);
+            // console.log(pokemon)
         })
         .catch((err) => console.log(err));
-    },[]);
+        console.log(pokemon)
+    },[search]);
+    const searchPokemon = (e) => {
+        e.preventDefault();
+        console.log(search);
+        // setPokemon('')
+        // freely type in the pokedex number without pressing search, no need to reset form
+    }
+    // const next = (e) => {
+    //     setSearch++;
+    // }
+    // const previous = (e) => {
+    //     setSearch--;
+    // }
     return (
         <div>
-            {   
+            <h1>Pokemon API</h1>
+            <form onSubmit={searchPokemon}>
+                <p>Use the Pokedex!</p>
+                <input type="number" value={search} required onChange={(e) => setSearch(e.target.value)}/>
+                {
+                    search > 898 || search < 1?
+                    <p>Pokedex is limitted to 1 - 898</p>:
+                    <>
+                    <p>Pokemon Name: {pokemon.name}</p>
+                    <p>Pokedex #{pokemon.id}</p>
+                    {/* make another api to display type to the matched pokemon id */}
+                    <p>Pokemon Weight: {pokemon.weight}</p>
+                    </>
+
+                }
+                {/* any number typed in form will automatically
+                render the page without search
+                */}
+                {/* <button type="submit">Search</button> */}
+            </form>
+                {/* Use if using search button */}
+                {
+                    search === 1 ?
+                    <button onClick={() => setSearch(search - 1)} disabled>previous</button>:
+                    <button onClick={() => setSearch(search - 1)}>previous</button>
+                }   
+                {
+                    search === 898 ?
+                    <button onClick={() => setSearch(search + 1)} disabled>Next</button>:
+                    <button onClick={() => setSearch(search + 1)}>Next</button>
+                }
+                
+
+            {/* {   if we want to read all objects from api
                 pokemon.map((pokemon, index) => {
                     return (
                         <li key={index}>{pokemon.name}</li>
                         
                     )
                 })
-            }
+            } */}
         </div>
     )
     // let [catchPokemon,setCatchPokemon] = useState(false);
