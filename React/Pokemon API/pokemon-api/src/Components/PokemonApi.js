fe5a160cde01735c98fdc92a3b55db7231f164ee
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-13 21:49:34 -0700

    implemented axios for api

--- React/Pokemon API/pokemon-api/src/Components/PokemonApi.js
+++ React/Pokemon API/pokemon-api/src/Components/PokemonApi.js
@@ -1,30 +1,56 @@
 import React, {useEffect, useState} from 'react';
-
+import axios from 'axios'
 const PokemonApi = (props) => {
-    const [pokemon,setPokemon] = useState([]);
 
-    useEffect(() => {
-            fetch('https://pokeapi.co/api/v2/pokemon')
-                .then(response => response.json())
-                .then(response => setPokemon(response.results))
-                .catch(err => console.log(err))
-        },[pokemon.catch]);
-    
+    const [pokemon,setPokemon] = useState([]);
+    useEffect(()=>{
+        axios
+        .get('https://pokeapi.co/api/v2/pokemon?limit=807')
+        // .then(response=>{setPokemon(response.data.results)})
+        .then((response) => {
+            setPokemon(response.data.results);
+            console.log(pokemon)
+        })
+        .catch((err) => console.log(err));
+    },[]);
     return (
-    <div>
-        <button checked={pokemon.catch}>Catch All Pokemons!</button>
-        <ol>
-        {   
-            pokemon.length > 0 && pokemon.map((pokemon, index) => {
-                return (
-                    <li key={index}>
-                        {pokemon.name}
-                    </li>
-                )
-            })
-        }
-        </ol>
-    </div>
-)
+        <div>
+            {   
+                pokemon.map((pokemon, index) => {
+                    return (
+                        <li key={index}>{pokemon.name}</li>
+                        
+                    )
+                })
+            }
+        </div>
+    )
+    // let [catchPokemon,setCatchPokemon] = useState(false);
+    // useEffect(() => {
+    //         fetch('https://pokeapi.co/api/v2/pokemon')
+    //             .then(response => response.json())
+    //             .then(response => setPokemon(response.results))
+    //             .catch(err => console.log(err))
+    //     },[]);
+
+    // const toggle = (e) => {
+    //     catchPokemon = !catchPokemon;
+    //     console.log(catchPokemon);
+    // }
+    // return (
+    // <div>
+    //     <button checked={catchPokemon} onClick={(e)=> toggle(e)}>Catch All Pokemons!</button>
+    //     <ol> 
+    //     {   
+    //         pokemon.length > 0 && pokemon.map((pokemon, index) => {
+    //             return (
+    //                 <li key={index}>{pokemon.name}</li>
+                    
+    //             )
+    //         })
+    //     }
+    //     </ol>
+    // </div>
+    // )
 }
 export  default PokemonApi;
