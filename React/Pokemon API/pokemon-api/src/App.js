08589ddc76069f69434f43bc58c677116d9936a2
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-14 19:57:13 -0700

    moved all code from component into app.js

--- React/Pokemon API/pokemon-api/src/App.js
+++ React/Pokemon API/pokemon-api/src/App.js
@@ -1,12 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
-import PokemonApi from './Components/PokemonApi';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios'
 function App() {
-  return (
-    <div className="App">
-      <PokemonApi/>
-    </div>
-  );
-}
+    const [pokemon,setPokemon] = useState({});
+    const [search,setSearch] = useState('');
+    useEffect(()=>{
+        axios
+        
+        .get(`https://pokeapi.co/api/v2/pokemon/${search}`)
+        // .then(response=>{setPokemon(response.data.results)})
+        .then((response) => {
+            setPokemon(response.data);
+            // console.log(pokemon)
+        })
+        .catch((err) => console.log(err));
+        console.log(pokemon)
+    },[search]);
+    const searchPokemon = (e) => {
+        e.preventDefault();
+        console.log(search);
+        // setPokemon('')
+        // freely type in the pokedex number without pressing search, no need to reset form
+    }
+    return (
+      <>
+        <div className='App'>
+          <h1>Pokemon API</h1>
+          <form onSubmit={searchPokemon}>
+              <p>Use the Pokedex!</p>
+              <input type="number" value={search} required onChange={(e) => setSearch(e.target.value)}/>
+              <button type="submit">Search</button>
+              {
+                  search > 898 || search < 1?
+                  <>
+                  <p>Search all 898 Pokemons in our Pokedex!</p>
+                  </>:
+                  
+                  <>
+                  <p>Pokemon Name: {pokemon.name}</p>
+                  <p>Pokedex #{pokemon.id}</p>
+                  {/* make another api to display type to the matched pokemon id */}
+                  <p>Pokemon Weight: {pokemon.weight}</p>
+                  </>
 
-export default App;
+              }
+              {/* any number typed in form will automatically
+              render the page without search
+              */}
+          </form>
+              {/* Use if using search button */}
+              {
+                  search === 0 || search === '' ?
+                  <button onClick={() => setSearch(search - 1)} disabled>previous</button>:
+                  <button onClick={() => setSearch(search - 1)}>previous</button>
+              }   
+              {
+                  search === 898 ?
+                  <button onClick={() => setSearch(search + 1)} disabled>Next</button>:
+                  <button onClick={() => setSearch(search + 1)}>Next</button>
+              }
+        </div>
+
+      </>
+    )
+  }
+
+export  default App;
