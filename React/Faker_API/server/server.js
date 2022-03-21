19ee59560069ff7cfc22557e0cd6de595ceef3ff
Jonel Briones <jonel.c.briones@gmail.com>
2022-03-21 09:51:37 -0700

    created variables for objects

--- React/Faker_API/server/server.js
+++ React/Faker_API/server/server.js
@@ -2,44 +2,64 @@ const express = require("express");
 const faker = require("faker");
 const app = express();
 
-app.get("/api/user",(required,response)=>{
+const createUser = () => {
     const id = Math.floor(Math.random() * 10000); 
     const firstName = faker.name.firstName();
     const lastName = faker.name.lastName();
     const phoneNumber = faker.phone.phoneNumber();
     const userEmail = faker.internet.email();
     const password = `${firstName + lastName + id}`; 
-    response.json({
+    const newUser = {
         id : id,
         firstName: firstName,
         lastName : lastName,
         phoneNumber : phoneNumber,
         userEmail : userEmail,
         password : password,
+    }
+    return newUser;
+}
+
+const fakeCreateUser = createUser();
+
+const createCompany = () => {
+    const id = Math.floor(Math.random() * 10000); 
+    const companyName = faker.commerce.productName();
+    const address =
+    {
+        street : faker.address.streetName(),
+        city : faker.address.cityName(),
+        state : faker.address.state(),
+        zipcode : faker.address.zipCode(),
+        country : faker.address.country(),
+    }
+
+    const newCompany = {
+        id : id,
+        companyName: companyName,
+        address : address,
+    }
+    return newCompany;
+}
+
+const fakeCreateCompany = createCompany();
+
+app.get("/api/user/new",(required,response)=>{
+    response.json({
+        fakeCreateUser
     })
 })
 
-app.get("/api/company", (req,res)=>{
-    const id = Math.floor(Math.random() * 10000); 
-    const companyName = faker.name.firstName();
-    const address = [
-        faker.name.firstName(),
-        faker.name.firstName(),
-        faker.name.firstName(),
-        Math.floor(Math.random() * 100000),
-        faker.name.firstName()
-    ]
+app.get("/api/company/new", (req,res)=>{
     res.json({
-        id : id,
-        companyName : companyName,
-        address : [
-            {street : address[0]},
-            {city : address[1]},
-            {state : address[2]},
-            {zipCode : address[3]},
-            {country : address[4]},
-        ]
-    });
-    
+        fakeCreateCompany
+    })
 });
+app.get("/api/user/company",(req,res)=>{
+    res.json({
+        fakeCreateCompany,
+        fakeCreateUser
+    })
+})
+
 app.listen(8000,()=>console.log("You have successfully connect to port 8000"));
