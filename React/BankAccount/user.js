afa64829919a4e363827b50daca2ece05da489a9
Jonel Briones <jonel.c.briones@gmail.com>
2022-04-08 21:18:17 -0700

    combined user and bankAccount classes

--- React/BankAccount/user.js
+++ React/BankAccount/user.js
@@ -1,36 +1,77 @@
+class BankAccount {
+    constructor(intRate,balance) {
+        this.intRate = intRate;
+        this.balance = balance + 0;
+    }
+    deposit(amount) {
+        this.balance += amount;
+        this.deposited = amount;
+        // console.log(`Deposited: $${this.deposited}`)
+        
+        return this;
+    }
+    withdraw(amount) {
+        this.balance -= amount;
+        this.withdrawed = amount;
+        // console.log(`Withdrawed: $${this.withdrawed}`)
 
+        return this;
+    }
+    displayAccountInfo(name) {
+
+        console.log(`${name}'s Account Balance: $${this.
+        intRate + this.balance}`)
+        console.log('----------------------')
+        return this;
+    }
+    yieldInterest() {
+        this.intRate *= this.balance;
+        console.log(`Interest Rate: $${this.intRate}`)
+
+        return this 
+    }
+}
 class User {
     constructor(username,email) {
         this.name = username
         this.email = email
-        this.accountBalance = 0
+        this.account = new BankAccount(0.02,0)
     }
     makeDeposit(amount) {
-        this.accountBalance += amount;
-        this.deposited = amount;
-        console.log(`${this.name} Deposited: $${this.deposited}`)
+        this.account.deposit(amount);
+        this.amount = amount
+        console.log(`${this.name} Deposited: $${this.amount}`)
         
         return this;
     }
     makeWithdraw(amount) {
-        this.accountBalance -= amount;
-        this.withdrawed = amount;
-        console.log(`${this.name} Withdrawed: $${this.withdrawed}`)
-
+        if(amount>this.account.balance) {
+            console.log("Insuffient Funds!: $",this.account.balance - amount)
+        } else
+        this.account.withdraw(amount);
+        this.amount = amount
+        console.log(`${this.name} Withdrawed: $${this.amount}`)
+        
         return this;
 
     }
     displayBalance() {
-        this.balance = this.accountBalance;
-        console.log(`${this.name} Account Balance: $${this.accountBalance}`)
+        this.account.displayAccountInfo(this.name);
         return this;
 
     }
     transferMoney(amount,user) {
-        this.accountBalance -= amount;
+        this.displayBalance()
+        user.displayBalance()
+        if(amount>this.account.balance) {
+            console.log("Insuffient Funds!: $",this.account.balance - amount)
+            return this
+        } 
+        else
+        this.account.balance -= amount;
         this.amount = amount;
 
-        user.accountBalance += amount;
+        user.account.balance += amount;
         console.log(`${this.name} transfered: $${this.amount} to ${user.name}`)
         return this;
 
@@ -46,10 +87,10 @@ monty.makeWithdraw(1000).displayBalance()
 const guido = new User("Jonathan", "jonathan@gmail.com");
 guido.makeDeposit(500).makeDeposit(1000).makeWithdraw(500).makeWithdraw(500).displayBalance()
 
-// transfer money from user to another user
-monty.displayBalance()
-guido.displayBalance()
-monty.transferMoney(200,guido)
+// // transfer money from user to another user
+console.log('transfer')
+
+monty.transferMoney(1000,guido)
 monty.displayBalance()
 guido.displayBalance()
 
