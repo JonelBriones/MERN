f5f140f0b6b120eebbf16b7f8560d247289a9a0a
Jonel Briones <jonel.c.briones@gmail.com>
2022-04-08 18:32:28 -0700

    updated with chaining method

--- React/BankAccount/user.js
+++ React/BankAccount/user.js
@@ -9,51 +9,49 @@ class User {
     makeDeposit(amount) {
         this.accountBalance += amount;
         this.deposited = amount;
+        console.log(`${this.name} Deposited: $${this.deposited}`)
+        
+        return this;
     }
     makeWithdraw(amount) {
         this.accountBalance -= amount;
         this.withdrawed = amount;
+        console.log(`${this.name} Withdrawed: $${this.withdrawed}`)
+
+        return this;
+
     }
     displayBalance() {
         this.balance = this.accountBalance;
+        console.log(`${this.name} Account Balance: $${this.accountBalance}`)
+        return this;
+
     }
     transferMoney(amount,user) {
         this.accountBalance -= amount;
         this.amount = amount;
 
         user.accountBalance += amount;
+        console.log(`${this.name} transfered: $${this.amount} to ${user.name}`)
+        return this;
+
     }
 }
+// user makes a deposit and withdraw 
 const monty = new User("Jonel","ijonel906@gmail.com");
 console.log(monty.name) // output: Jonel
-monty.makeDeposit(1000)
-console.log(`${monty.name} deposited: ${monty.deposited}`)
-monty.makeDeposit(1000)
-console.log(`${monty.name} deposited: ${monty.deposited}`)
-monty.makeDeposit(1000)
-console.log(`${monty.name} deposited: ${monty.deposited}`)
-monty.makeWithdraw(500)
-console.log(`${monty.name} withdrawed: ${monty.withdrawed}`)
-monty.displayBalance()
-console.log(`${monty.name} account balance: ${monty.balance}`)
+monty.makeDeposit(1000).makeDeposit(1000).makeDeposit(1000).displayBalance()
+monty.makeWithdraw(1000).displayBalance()
 
-
+// second user makes a deposit and withdraw 
 const guido = new User("Jonathan", "jonathan@gmail.com");
-guido.makeDeposit(500)
-console.log(`${guido.name} deposited: ${guido.deposited}`)
-guido.makeDeposit(1000)
-console.log(`${guido.name} deposited: ${guido.deposited}`)
-guido.makeWithdraw(500)
-console.log(`${guido.name} withdrawed: ${guido.withdrawed}`)
-guido.makeWithdraw(500)
-console.log(`${guido.name} withdrawed: ${guido.withdrawed}`)
-guido.displayBalance()
-console.log(`${guido.name} account balance: ${guido.balance}`)
+guido.makeDeposit(500).makeDeposit(1000).makeWithdraw(500).makeWithdraw(500).displayBalance()
 
-console.log(`${monty.name} account balance: ${monty.balance}`)
-console.log(`${guido.name} account balance: ${guido.balance}`)
+// transfer money from user to another user
+monty.displayBalance()
+guido.displayBalance()
 monty.transferMoney(200,guido)
-console.log(`${monty.name} transfered: $${monty.amount} to ${guido.name}`)
-console.log(`${monty.name} account balance: ${monty.accountBalance}`)
-console.log(`${guido.name} account balance: ${guido.accountBalance}`)
+monty.displayBalance()
+guido.displayBalance()
+
 
