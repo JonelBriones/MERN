b8b241546773934b3ac542d13fd6f7c13d1fa5de
Jonel Briones <jonel.c.briones@gmail.com>
2022-04-10 01:54:41 -0700

    added state to class component

--- React/create_react_app/src/components/PersonCard.js
+++ React/create_react_app/src/components/PersonCard.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 class PersonCard extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            count: 0
+        }
+    }
+    birthday() {
+        this.setState({count: this.state.count + 1})
+        return this;
+    }
     render() {
         const {firstName,lastName,age,hairColor} = this.props;
         
         return (
         <div>
             <h1>{firstName}, {lastName}</h1>
-            <p>Age: {age}</p>
+            <p>Age: {age + this.state.count}</p>
             <p>Hair Color: {hairColor}</p>
             
-            {/* <button onClick={count()}>Birthday Button for {firstName} {lastName}</button>
-            <button onClick={ ()=> alert(`${firstName} turned ${age + count}!`)}>Alert Click Count</button> */}
+            <button onClick={()=>this.birthday()}>Birthday Button for {firstName} {lastName}</button>
+            <button onClick={ ()=> alert(`${firstName} turned ${age + this.state.count}!`)}>Alert Click Count</button>
         </div>
         )
     }   
