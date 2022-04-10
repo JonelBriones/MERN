6cd8a8194b0756443e49b7528b2f204ef7502f3c
Jonel Briones <jonel.c.briones@gmail.com>
2022-04-10 00:37:27 -0700

    changed function to class component

--- React/create_react_app/src/components/PersonCard.js
+++ React/create_react_app/src/components/PersonCard.js
@@ -1,29 +1,18 @@
-import React, { useState} from 'react';
-// useState is a built in Hook that is temporarily 
-// components are what links data to our front end / html
-// props short for properties are how we call in data's keys : values
-const PersonCard = (props) => {
-    const [count, setCount] = useState(0); // 0 is our starting value
-    
-    // variable handleClick will be return the same result as count + 1 
-    // const handleClick = () => {
-    //     setCount(count + 1);
-    // }
-
-    // destructuring our props into local variables
-    // {props.age} == {props}
-    // example {age, firstName, lastName, hairColor,} == {props.etc}
-    const {age} = props;
-    return (
-    <div>
-        <h1>{props.firstName}, {props.lastName}</h1>
-        <p>Age: {age + count}</p>
-        <p>Hair Color: {props.hairColor}</p>
+import React, { Component } from 'react';
+class PersonCard extends Component {
+    render() {
+        const {firstName,lastName,age,hairColor} = this.props;
         
-        <button onClick={() => setCount(count + 1)}>Birthday Button for {props.firstName} {props.lastName}</button>
-        <button onClick={ ()=> alert(`${props.firstName} turned ${age + count}!`)}>Alert Click Count</button>
-    </div>
-    )
-        
+        return (
+        <div>
+            <h1>{firstName}, {lastName}</h1>
+            <p>Age: {age}</p>
+            <p>Hair Color: {hairColor}</p>
+            
+            {/* <button onClick={count()}>Birthday Button for {firstName} {lastName}</button>
+            <button onClick={ ()=> alert(`${firstName} turned ${age + count}!`)}>Alert Click Count</button> */}
+        </div>
+        )
+    }   
 }
 export default PersonCard;
