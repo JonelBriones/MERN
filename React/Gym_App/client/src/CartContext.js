5defa3003a9881c40b43a67c650a90157022a31c
Jonel Briones <jonel.c.briones@gmail.com>
2022-04-24 00:33:47 -0700

    global added to cart state

--- React/Gym_App/client/src/CartContext.js
+++ React/Gym_App/client/src/CartContext.js
@@ -4,7 +4,7 @@ const CartContext = createContext();
 
 export function CartProvider({children}) {
     const [cart,setCart] = useState([])
-
+    const [cartQty,setQty] = useState(0)
     const addToCart = (productObject) => {
 
         // is our product already in the cart?
@@ -26,6 +26,8 @@ export function CartProvider({children}) {
                 */
             ))
             console.log(cart)
+            // setQty(cartQty+1
+
         }
         // else, add to cart and iniate item quantity key value pair
         else {
@@ -39,7 +41,9 @@ export function CartProvider({children}) {
                     }
                 */
             console.log(cart)
+            
         }
+        setQty(cartQty+1)
     }
     const removeFromCart = (productObject) => {
         const exist = cart.find((product)=>product._id === productObject._id)
@@ -66,9 +70,30 @@ export function CartProvider({children}) {
                 ))
             console.log(cart)
         }
+        setQty(cartQty-1)
     }
+
+    // const qty = () => {
+    //     // loop through products in cart
+    //     // each product.qty at index is added to total qty
+    //     let sum = 0;
+    //     for(let i = 0; i<cart.length; i++) {
+    //         // if(cart[i]){
+    //         //     console.log("product",[i],cart[i])
+    //         //     console.log("previously",qty)
+    //         //     console.log("added",product.qty)
+    //         // }
+    //         let product = cart[i]
+    //         console.log(sum,product.qty)
+    //         sum += product.qty;
+    //         setQty(sum)
+    //     }
+    //     console.log("qty",sum)
+    //     // return sum
+    // }
+
     return (
-        <CartContext.Provider value={{item:1,cart,addToCart,removeFromCart}}>{children}</CartContext.Provider>
+        <CartContext.Provider value={{item:1,cart,addToCart,removeFromCart,cartQty}}>{children}</CartContext.Provider>
     )
 }
 
